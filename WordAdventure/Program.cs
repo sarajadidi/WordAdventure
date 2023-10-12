bc72dd1d455298ac59790bd2ac2a33a01a6ac97f
sarajadidi <sararivkajadidi@gmail.com>
2023-10-12 08:36:33 -0700

    initial commit

---
+++ WordAdventure/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("   __            _ _     _ _     ___                          ");
+Console.WriteLine("   \\ \\  __ _  __| (_) __| (_)   / _ \\__ _ _ __ ___   ___  ___ ");
+Console.WriteLine("    \\ \\/ _` |/ _` | |/ _` | |  / /_\\/ _` | '_ ` _ \\ / _ \\/ __|");
+Console.WriteLine(" /\\_/ / (_| | (_| | | (_| | | / /_\\\\ (_| | | | | | |  __/\\__ \\");
+Console.WriteLine(" \\___/ \\__,_|\\__,_|_|\\__,_|_| \\____/\\__,_|_| |_| |_|\\___||___/");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+
+Console.WriteLine("Please enter your name.");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello, {name}! Welcome to your very own Word Adventure Game!");
+Console.WriteLine("Imagine, you are a young child at the age of 12 years old. ");
+Console.WriteLine("You live in a town where your parents rule and protect. You have one sister, ");
+Console.WriteLine( "3 brothers and a half brother who you have a special siblingly bond with.  ");
+Console.WriteLine();
+Console.WriteLine();
+
