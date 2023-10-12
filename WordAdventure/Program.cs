0c436fab010ec10285376ba1a45a3b8bae974c2c
sarajadidi <sararivkajadidi@gmail.com>
2023-10-12 09:40:44 -0700

    worked a bit

--- WordAdventure/Program.cs
+++ WordAdventure/Program.cs
@@ -19,6 +19,17 @@ Console.WriteLine($"Hello, {name}! Welcome to your very own Word Adventure Game!
 Console.WriteLine("Imagine, you are a young child at the age of 12 years old. ");
 Console.WriteLine("You live in a town where your parents rule and protect. You have one sister, ");
 Console.WriteLine( "3 brothers and a half brother who you have a special siblingly bond with.  ");
-Console.WriteLine();
-Console.WriteLine();
+Console.WriteLine("You Live a great life, truly! But you've always had a desire for some real adventure.");
+Console.WriteLine("You usually stay inside, reading, or helping your Mother and the servents cook,");
+Console.WriteLine("but today you wanted to create some chaos...");
+
+Console.WriteLine("Do you want to go out and pick up the bow and shoot some arrows?");
+Console.WriteLine("OR");
+Console.WriteLine("Climb the Lighthouse?");
+Console.WriteLine("Please enter 'climb' or 'shoot'.");
+
+string chaosDecision = Console.ReadLine();
+
+string chaosInput = "climb";
+string chaosAnswer = "shoot";
 
