53c0eea87a893d5c74a99cdd0334373d9c3f30a2
sarajadidi <sararivkajadidi@gmail.com>
2023-10-12 10:19:30 -0700

    added first switch case

--- WordAdventure/Program.cs
+++ WordAdventure/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using System.Collections.Generic;
+using System.Security.Claims;
 
 Console.ForegroundColor = ConsoleColor.DarkBlue;
 Console.WriteLine("   __            _ _     _ _     ___                          ");
@@ -18,8 +19,8 @@ string name = Console.ReadLine();
 Console.WriteLine($"Hello, {name}! Welcome to your very own Word Adventure Game!");
 Console.WriteLine("Imagine, you are a young child at the age of 12 years old. ");
 Console.WriteLine("You live in a town where your parents rule and protect. You have one sister, ");
-Console.WriteLine( "3 brothers and a half brother who you have a special siblingly bond with.  ");
-Console.WriteLine("You Live a great life, truly! But you've always had a desire for some real adventure.");
+Console.WriteLine( "three brothers and a half brother who you have a special siblingly bond with.  ");
+Console.WriteLine("You live a great life, truly! But you've always had a desire for some real adventure.");
 Console.WriteLine("You usually stay inside, reading, or helping your Mother and the servents cook,");
 Console.WriteLine("but today you wanted to create some chaos...");
 
@@ -30,6 +31,29 @@ Console.WriteLine("Please enter 'climb' or 'shoot'.");
 
 string chaosDecision = Console.ReadLine();
 
-string chaosInput = "climb";
-string chaosAnswer = "shoot";
+switch (chaosDecision.ToLower())
+{
+    case "climb":
+        Console.WriteLine("You see the Lighthouse from the cobblestone floor of the castle center,");
+        Console.WriteLine("The urge to go climb it like you did when you were smaller took over.");
+        Console.WriteLine("Before you knew it you had your hands and feet on the wall and you could");
+        Console.WriteLine("see the locals below going about their buisness. Off in the distance,  ");
+        Console.WriteLine("you see a carriage riding towards your little town.  ");
+        break;
+    case "shoot":
+        Console.WriteLine("You race out of the castle and outside to the range where your brothers are sparring.");
+        Console.WriteLine("Hidden, you grab a bow from the chest behind the brick wall that stands between you and ");
+        Console.WriteLine("your brothers, you set up your bow with the arrow. Still hidden, you peer over the wall ");
+        Console.WriteLine("and let it fly. The arrow cuts through the air and lands on that little dot in the center ");
+        Console.WriteLine("of the bullseye. In awe of yourself, you stand there, speechless, while all of your brothers, " +
+            "\nas well as your father turn around to see you standing their with your bow. All in utter shock.");
+        
+        break;
+    default:
+        Console.WriteLine("Invalid choice, Please try again");
+        break;
+}
+
+
+
 
